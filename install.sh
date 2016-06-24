a2cb3d57f78c39662377e245b1aedc7ca2822c57
Pawel Przytula <pawel@appsilon.pl>
2016-06-24 13:23:04 +0200

    Fix in install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,12 @@
 #/bin/bash
 
-DATAFLOWS=`pwd`/dataflows
-
-pip install -r requirements.txt
-
-if [ -e $DATAFLOWS ]; then
-    ln -s $DATAFLOWS /usr/bin/dataflows
+if [ -e $DATAFLOWS_DIR ]; then
+    cd $DATAFLOWS_DIR
+    pip install -r requirements.txt
+    ln -s $DATAFLOWS_DIR /usr/bin/dataflows
 else 
     echo "Dataflows executable does not exist"
+    exit(1)
 fi 
 
 
